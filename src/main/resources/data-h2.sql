8e573c50fe979cde1c3249ab3c9daa7d31c39792
DESKTOP-QA5O2KU\samsung <hshlego@naver.com>
2022-01-20 01:11:28 +0900

    no test for me

--- src/main/resources/data-h2.sql
+++
@@ -1,8 +0,0 @@
-create table SNSDATATEST(
-	media varchar(255) primary key,
-	content varchar(255),
-	url text,
-	time varchar(255)
-);
-insert into SNSDataTest (media, content, url, time) values ('1', '1', '1', '1');
-insert into SNSDataTest (media, content, url, time) values ('2', '2', '2', '2');
--- src/test/java/com/mincho/electionpjt/domain/SNSDataRepositoryTest.java
+++
@@ -1,6 +0,0 @@
-package com.mincho.electionpjt.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SNSDataRepositoryTest extends JpaRepository<SNSData, Long>{
-}
